8b31f5326ca2ced439c674f551919855def5b379
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2024-06-16 19:32:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 import time
+import json
 try:
     import pyautogui
     # pip install opencv-python
 except ImportError:
     os.system("pip install pyautogui")
     os.system("pip install opencv-python")
-
-print("Auto Clicker Activated!")
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+print(f"Auto Clicker Activated! Waiting for the '{config["button"]}' button.")
 while True:
-    autonext = pyautogui.locateCenterOnScreen('next.png', confidence=0.75)
-    if autonext:
-        pyautogui.moveTo(autonext)
+    if config["button"]  == "Next":
+        button = pyautogui.locateCenterOnScreen('next.png', confidence=0.75)
+    else
+        button = pyautogui.locateCenterOnScreen('replay.png', confidence=0.75)
+    if button:
+        pyautogui.moveTo(button)
         time.sleep(0.06)
         pyautogui.click()
-        print(f"Clicked 'next' button at {autonext}")
+        print(f"Clicked '{config["button"]}' button at {button}")
     else:
         time.sleep(0.1)
